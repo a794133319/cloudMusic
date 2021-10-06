e6d1eec31b078c93add9db62e208a9849f99f66c
a794133319 <55074811+a794133319@users.noreply.github.com>
2021-10-06 11:01:04 +0800

    Update cloudMusic.js

--- js/cloudMusic.js
+++ js/cloudMusic.js
@@ -3,7 +3,14 @@ var app = new Vue({
 	data:{
 		musicName:"",
 		musicList:[],
-		musicUrl:""
+		musicUrl:"",
+		musicPicUrl:"",
+		isPlaying:false,
+		commentsList:[]
+	},
+	mounted:function(){
+		this.musicName = "爱情";
+		this.seachMusic();
 	},
 	methods:{
 		//搜索音乐
@@ -16,13 +23,29 @@ var app = new Vue({
 				that.musicList = response.data.result.songs;
 			},function(err){})
 		},
+		// 播放音乐
 		playMusic:function(musicId){
 			var that = this;
+			this.isPlaying = true;
+			// 获取音乐的url
 			axios.get("https://autumnfish.cn/song/url?id=" + musicId).then(
 			function(response){
-				console.log(response);
+				// console.log(response);
 				that.musicUrl = response.data.data[0].url;
 			},function(err){})
+			//获取音乐的封面
+			axios.get("https://autumnfish.cn/song/detail?ids=" + musicId).then(
+			function(response){
+				console.log(response);
+				that.musicPicUrl = response.data.songs[0].al.picUrl;
+			},function(err){})
+			// 获取评论列表
+			axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + musicId).then(
+			function(response){
+				console.log(response);
+				that.commentsList = response.data.hotComments;
+			},function(err){})
+			
 		}
 	}
 })
